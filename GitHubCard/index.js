581077244d169e4fc7e1046ca8ec605db7a73472
Blayze <blayzestone@protonmail.com>
2020-04-09 14:31:49 -0700

    feat: create card maker function
    The card maker function creates an HTML component of github user data
    passed to the it.
    
    create a card using the cardMaker function and append it to the index
    page.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,17 +2,24 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cardsContainer = document.querySelector(".cards");
+axios.get('https://api.github.com/users/blayzestone')
+  .then(response => response.data)
+  .then(data => {
+    /* Step 2: Inspect and study the data coming back, this is YOUR 
+      github info! You will need to understand the structure of this 
+      data in order to use it to build your component function 
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+      Skip to Step 3.
+    */
+   console.log(data);
 
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+    /* Step 4: Pass the data received from Github into your function, 
+              create a new component and add it to the DOM as a child of .cards
+    */
+    const card = cardMaker(data);
+    cardsContainer.appendChild(card);    
+  });
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -45,6 +52,54 @@ const followersArray = [];
 </div>
 
 */
+function cardMaker(userData) {
+  // Create elements
+  const card = document.createElement("div");
+  const cardImage = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const address = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  // Add classes
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  // set attributes
+  cardImage.src = userData.avatar_url;
+  address.href = userData.url;
+
+  // set text content
+  name.textContent = userData.name;
+  username.textContent = userData.login;
+  location.textContent = userData.location;
+  profile.textContent = "Profile ";
+  address.textContent = userData.url;
+  followers.textContent = `Followers: ${userData.followers}`;
+  following.textContent = `Following: ${userData.following}`;
+  bio.textContent = `Bio: ${userData.bio}`;
+
+  // append elements
+  card.appendChild(cardImage);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+    profile.appendChild(address);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
